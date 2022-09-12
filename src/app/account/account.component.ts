34f9deb322944f26670188e0d02bbd6d1d0bf099
Calebe1100 <calebebraga08@gmail.com>
2022-09-12 20:37:59 -0300

    create login

---
+++ src/app/account/account.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-account',
+  templateUrl: './account.component.html',
+  styleUrls: [ './account.component.scss' ]
+})
+export class AccountComponent  {
+  loginForm: FormGroup;
+  constructor() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required])
+    });
+  }
+
+  onSubmit() {
+    if(this.loginForm.valid) {
+      console.log(this._v());
+    }
+  }
+  _v() {
+    return this.loginForm.value;
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccountComponent } from './account/account.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AccountModule } from './account/account.module';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    AccountModule
   ],
   providers: [],
   bootstrap: [AppComponent]
