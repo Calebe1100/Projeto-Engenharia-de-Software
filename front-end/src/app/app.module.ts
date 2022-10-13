c9e18c6fc662a36721f07245f4dfc887bebcce74
Calebe Libério <calebe.liberio@vertown.com>
2022-10-12 23:21:44 -0300

    fix eslint and prettier

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,17 +1,15 @@
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { PagesModule } from './pages/pages.module';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { PagesModule } from "./pages/pages.module";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     PagesModule,
     BrowserModule,
@@ -22,6 +20,6 @@ import { PagesModule } from './pages/pages.module';
   ],
   providers: [],
   exports: [PagesModule],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
