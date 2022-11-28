3ba31170dac5e353f8828f97af54e72c033c1a90
Calebe Libério <calebe.liberio@vertown.com>
2022-11-28 16:32:25 -0300

    fix layout and mock

--- front-end/src/app/pages/dialog/dialog-view-discipline/dialog-view-discipline.component.ts
+++ front-end/src/app/pages/dialog/dialog-view-discipline/dialog-view-discipline.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgxFlowChatOptions, NgxFlowChatData } from 'ngx-flowchart'
+import { NgxFlowChatOptions, NgxFlowChatData } from 'ngx-flowchart';
 import { DisciplinesFlowService } from 'src/services/api/disciplines-flow/disciplines-flow.service';
 import { SystemDiscipline } from 'src/services/api/system-disciplines/interface/SystemDiscipline';
 
@@ -15,48 +15,62 @@ export class DialogViewDisciplineComponent implements OnInit {
   sampleNodes = [
     {
       id: 'start',
-      label: 'start'
-    }, {
-        id: '1',
-        label: 'Query ThreatConnect',
-        rank: 'first'
-    }, {
-        id: '2',
-        label: 'Query XForce',
-        rank: 'first'
-    }, {
-        id: '3',
-        label: 'Format Results'
-    }, {
-        id: '4',
-        label: 'Search Splunk'
-    }, {
-        id: '5',
-        label: 'Block LDAP'
-    }, {
-        id: '6',
-        label: 'Email Results'
-    }];
+      label: 'Cálculo 1',
+    },
+    {
+      id: '1',
+      label: 'Cálculo 2',
+      rank: 'first',
+    },
+    {
+      id: '2',
+      label: 'Cálculo 3',
+      rank: 'first',
+    },
+    {
+      id: '3',
+      label: 'Cálculo 4',
+    },
+    {
+      id: '4',
+      label: 'Controle Digital',
+    },
+    {
+      id: '5',
+      label: 'Lab Controle Digital',
+    },
+    {
+      id: '6',
+      label: 'IA',
+    },
+  ];
 
-  sampleLinks = [{
+  sampleLinks = [
+    {
       source: 'start',
-      target: '1'
-  }, {
+      target: '1',
+    },
+    {
       source: 'start',
-      target: '2'
-  }, {
+      target: '2',
+    },
+    {
       source: '1',
-      target: '3'
-  }, {
+      target: '3',
+    },
+    {
       source: '2',
-      target: '4'
-  }, {
+      target: '4',
+    },
+    {
       source: '2',
-      target: '6'
-  }, {
+      target: '6',
+    },
+    {
       source: '3',
-      target: '5'
-  }];
+      target: '5',
+    },
+  ];
 
   clickN(event: any) {
     console.log(event);
