0e057fff475c147a40537914807701bd8720df90
Calebe Libério <calebe.liberio@vertown.com>
2023-07-29 14:12:59 -0300

    create component

---
+++ front-end/src/app/shared/components/popup/popup.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PopupComponent } from './popup.component';
+
+@NgModule({
+  declarations: [PopupComponent],
+  imports: [CommonModule],
+  exports: [PopupComponent],
+})
+export class BasicSnackbarModule {}
